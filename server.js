a46ae67252fa38a1cbe209d4401e0f558935770e
MirSzabo <poradova.m@gmail.com>
2020-01-02 23:13:20 +0100

    handle production

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ app.use(express.static(path.join(__dirname, "public")));
 //routes
 app.use("/api/v1/stores", require("./routes/stores"));
 
+//handle production
+if(process.env.NODE_ENV === "production") {
+    //static folder
+    app.use(express.static(__dirname + "/public/"));
+
+    //handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.htm"));
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
